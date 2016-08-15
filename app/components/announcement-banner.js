8581be9b03d0b2c37c2338eccce9ce7dfc5f96a0
August von Trapp and Joel Delight <joel@>
2016-08-15 15:36:59 -0700

    style announcement-banner and add footer in application.hbs

--- app/components/announcement-banner.js
+++ app/components/announcement-banner.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  display: true,
+  actions: {
+    hide() {
+      this.set('display', false);
+    }
+  }
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      announcements: this.store.query('announcement', {orderBy: 'priority', equalTo: 1})
+    });
+  },
 });
